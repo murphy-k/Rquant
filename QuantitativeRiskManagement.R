b4a3a8a2e0c542d4cc43a240e4f614e708bedbb9
murphy-k <kyleevanmurphy@gmail.com>
2018-07-12 13:49:20 -0400

    1. self-explanatory

--- QuantitativeRiskManagement.R
+++ QuantitativeRiskManagement.R
@@ -1,4 +1,18 @@
-# Credit Risk Modeling ####
+# Packages ####
+library(quantmod)
+library(qrmdata)
+library(qrmtools)
 
 rm(list = ls())
 dev.off(dev.list()["RStudioGD"])
+
+
+# Data Series ####
+getSymbols("^DJI",
+           src = "yahoo",
+           auto.assign = TRUE,
+           from = "1900-01-01")
+
+DJI_dailyret <- dailyReturn(DJI$DJI.Close)
+DJI_weeklyret <- weeklyReturn(DJI$DJI.Close)
+DJI_monthlyret <- monthlyReturn(DJI$DJI.Close)
