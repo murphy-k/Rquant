86155d7ae6aa71f35b9442c6742542714738949b
murphy-k <kyleevanmurphy@gmail.com>
2018-11-10 00:10:53 -0500

    Deleted uneccesary practice files
    Moved crude oil into 'Forecasting Models'
    
    Moved 'Workflow_PortfolioAnalytics' back into Portfolio Analysis

--- EconometricsIdeas.R
+++
@@ -1,7 +0,0 @@
-# Econometrics Ideas
-
-# Will plotting an instruments 1.5x P/S ratio and 0.3x P/S ratio give me an
-# idea of where it’s stock should be valued at any given time ?
-
-# Can I systematically take valuation bands (P/S, P/FCF) and say “given 
-# where the rest of similar companies are - here is where x stands" ?
