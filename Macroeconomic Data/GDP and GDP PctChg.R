725b629b7d813dbe177db7cf5fac2becb36bf31e
murphy-k <kyleevanmurphy@gmail.com>
2018-12-08 16:12:28 -0500

    Cleaned up more duplicate code.
    Fixed forecasting length variables to always call the difference in length() between the full series and the training window.

--- Macroeconomic Data/GDP and GDP PctChg.R
+++ Macroeconomic Data/GDP and GDP PctChg.R
@@ -2,8 +2,6 @@
 library(fpp2)
 library(quantmod)
 library(forecast)
-# Clear environment & plots ####
-
 rm(list = ls())
 dev.off(dev.list()["RStudioGD"])
 
@@ -11,27 +9,22 @@ dev.off(dev.list()["RStudioGD"])
 getSymbols("GDPC1",
            src = "FRED",
            auto.assign = TRUE)
-GDP_Growth <-
-  getSymbols("A191RL1Q225SBEA", src = "FRED", auto.assign = FALSE)
 # Data Visualization ####
-chart_Series(GDP_Growth)
-
 chart_Series(GDPC1)
 gglagplot(GDPC1)
 ggAcf(GDPC1, lag.max = 100)
 
 pctchg_GDP <- ROC(GDPC1, n = 1, type = "continuous")
+pctchg_GDP_ts <- as.ts(pctchg_GDP)
 chart_Series(pctchg_GDP)
 
-GDP_Growth_ts <- as.ts(GDP_Growth)
 # Ljung-Box Test ####
 # White Noise describes purely random data. "Ljung-Box" test can confirm the
 # randomness of a series.
 # p-value > 0.05 = suggests NO difference from white noise
 # p-value < 0.05 = suggests difference from white noise
-
 options(scipen = 999)
-Box.test(GDP_Growth, lag = 10, type = "Ljung")
+Box.test(pctchg_GDP, lag = 10, type = "Ljung")
 Box.test(GDPC1, lag = 10, type = "Ljung")
 
 # Naive Forecast ####
@@ -40,29 +33,29 @@ Box.test(GDPC1, lag = 10, type = "Ljung")
 # naive forecast and is a useful benchmark for other forecasting methods.
 
 # Using the pipe function
-GDP_Growth_ts %>% naive(h = 10) -> naive_fc
+pctchg_GDP_ts %>% na.trim() %>% naive(h = 10) -> naive_fc
 autoplot(naive_fc)
 checkresiduals(naive_fc)
 
 # Subset for Training/Testing ####
-length(GDP_Growth_ts)
-length(GDP_Growth_ts) - (length(GDP_Growth_ts)*0.8)
-train1_GDP_Growth_ts <- subset(GDP_Growth_ts, start = 0,  end = 228)
-autoplot(train1_GDP_Growth_ts)
-
+length(pctchg_GDP_ts)
+train1_pctchg_GDP_ts <-
+  subset(pctchg_GDP_ts, end = length(pctchg_GDP_ts) * 0.8)
+autoplot(train1_pctchg_GDP_ts)
+h = length(pctchg_GDP_ts) - length(train1_pctchg_GDP_ts)
 # Model Training ####
-train_naive_fc <- naive(train1_GDP_Growth_ts, h = 57)
-train_mean_fc <- meanf(train1_GDP_Growth_ts, h = 57)
-train_ses_fc <- ses(train1_GDP_Growth_ts, h = 57)
+train_naive_fc <- naive(train1_pctchg_GDP_ts, h = h) 
+train_mean_fc <- meanf(train1_pctchg_GDP_ts, h = h)
+train_ses_fc <- ses(train1_pctchg_GDP_ts, h = h)
 
 autoplot(train_naive_fc) +
-  autolayer(GDP_Growth_ts, color = "black")
+  autolayer(pctchg_GDP_ts, color = "black")
 autoplot(train_mean_fc) +
-  autolayer(GDP_Growth_ts, color = "black")
+  autolayer(pctchg_GDP_ts, color = "black")
 autoplot(train_ses_fc) +
-  autolayer(GDP_Growth_ts)
+  autolayer(pctchg_GDP_ts)
 
 # Model Evaluation ####
-accuracy(train_naive_fc, GDP_Growth_ts)["Test set", c("MAPE", "RMSE")]
-accuracy(train_mean_fc, GDP_Growth_ts)["Test set", c("MAPE", "RMSE")]
-accuracy(train_ses_fc, GDP_Growth_ts)["Test set", c("MAPE", "RMSE")]
+accuracy(train_naive_fc, pctchg_GDP_ts)["Test set", c("MAPE", "RMSE")]
+accuracy(train_mean_fc, pctchg_GDP_ts)["Test set", c("MAPE", "RMSE")]
+accuracy(train_ses_fc, pctchg_GDP_ts)["Test set", c("MAPE", "RMSE")]
