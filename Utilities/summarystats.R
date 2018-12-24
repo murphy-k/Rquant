86a9456394713b2ba241d9e20da8a0607afe9baf
murphy-k <kyleevanmurphy@gmail.com>
2018-12-24 14:40:41 -0500

    Re-ran

--- Utilities/summarystats.R
+++ Utilities/summarystats.R
@@ -1,16 +1,19 @@
-  library(quantmod)
-  x <- getSymbols("FNGU", src="yahoo", auto.assign = FALSE)
-  x_adj <- x$FNGU.Adjusted
-  x_ROC <- na.trim(ROC(x_adj,n=1,type="continuous"))
-  
-  summary(x_ROC)
-  quantile(na.trim(x_ROC))
-  
-  plot(x_ROC)
-  hist(x_ROC,breaks = 100,probability = TRUE)
-  
-  x_mean <- mean(x_ROC)
-  x_sd <- sd(x_ROC)
-  z_score <- ((0.055) - x_mean) / x_sd
-  z_score
+rm(list=ls())
+
+library(quantmod)
+x <- getSymbols("YCS", src = "yahoo", auto.assign = FALSE)
+x_adj <- x$YCS.Adjusted
+x_ROC <- na.trim(ROC(x_adj, n = 1, type = "continuous"))
+
+summary(x_ROC)
+quantile(na.trim(x_ROC))
+
+plot(x_ROC)
+hist(x_ROC, breaks = 100, probability = TRUE)
+
+x_mean <- mean(x_ROC)
+x_sd <- sd(x_ROC)
+last(x_ROC)
+z_score <- ((last(x_ROC)) - x_mean) / x_sd
+z_score
 pnorm(z_score, lower.tail = FALSE)
