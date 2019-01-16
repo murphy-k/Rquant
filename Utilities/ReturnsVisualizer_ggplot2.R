d7b677bf37df741dc29201130efe92ed1df112ed
murphy-k <kyleevanmurphy@gmail.com>
2019-01-16 12:06:22 -0500

    Removed uneccesary variable declarations and folded them into the ggplot grammar.
    Added an argument to the zscore function to input a specific percentage return to calculate pnorm with

--- Utilities/ReturnsVisualizer_ggplot2.R
+++ Utilities/ReturnsVisualizer_ggplot2.R
@@ -1,67 +1,66 @@
 # ggplot2 Returns Visualizing workspace
 rm(list = ls())
 dev.off(dev.list()["RStudioGD"])
-
 library(dplyr)
 library(quantmod)
 library(ggplot2)
 library(magrittr)
+
 # Data Download
+ticker <- "DAN"
+lookback <- 100
+getSymbols(ticker,
+           src = "yahoo",
+           from = "2007-01-01",
+           warnings = FALSE)
 
-getSymbols("SPY",
-           src = "yahoo", 
-           from = "2018-01-01")
+# Subset data
+x <-
+  window(get(ticker), start = Sys.Date() - lookback, end = Sys.Date())
+chartSeries(x)
 
-
-# Structure and head()
-str(SPY)
-head(SPY)
-chartSeries(SPY)
 # Convert returns to DailyReturn percentage
-SPY_ret <- dailyReturn(SPY)
-SPY_meanReturn <- round(mean(SPY_ret), digits = 5)
-SPY_sdReturn <- round(sd(SPY_ret), digits = 5)
-plus1sd <- SPY_meanReturn + SPY_sdReturn
-minus1sd <- SPY_meanReturn - SPY_sdReturn
-plus2sd <- SPY_meanReturn + (SPY_sdReturn * 2)
-minus2sd <- SPY_meanReturn - (SPY_sdReturn * 2)
-SPY_ret <- as.xts(SPY_ret)
-
-
+x_ret <- dailyReturn(x)
+x_ret <- as.xts(x_ret)
+pacf(x_ret, lag.max = 100)
+# View Returns as a line plot
+ggplot(data = x_ret, aes(x = Index , y = x_ret$daily.returns)) +
+  geom_line() +
+  geom_hline(yintercept = mean(x_ret),
+             color = "blue",
+             linetype = "dashed") +
+  geom_hline(
+    yintercept = c(
+      mean(x_ret) + sd(x_ret),
+      mean(x_ret) + (sd(x_ret) * 2),
+      mean(x_ret) - sd(x_ret),
+      mean(x_ret) - (sd(x_ret) * 2)
+    ),
+    color = "black",
+    linetype = "solid"
+  )
 
 # View returns as a histogram
-ggplot(data = SPY_ret, aes(SPY_ret$daily.returns)) +
+ggplot(data = x_ret, aes(x_ret$daily.returns)) +
   geom_histogram(bins = 200,
                  aes(y = ..density..),
                  colour = "black",
                  fill = "white") +
-  #geom_density(alpha=.2, fill="white") +
+  geom_density(alpha = .2, fill = "white") +
   geom_vline(
-    aes(xintercept = mean(SPY_ret$daily.returns)),
+    aes(xintercept = mean(x_ret$daily.returns)),
     color = "blue",
     linetype = "dashed",
     size = 1
   ) +
-  geom_vline(aes(xintercept = plus1sd)) +
-  geom_vline(aes(xintercept = minus1sd)) +
-  geom_vline(aes(xintercept = plus2sd)) +
-  geom_vline(aes(xintercept = minus2sd))
+  geom_vline(aes(xintercept = mean(x_ret) + sd(x_ret))) +
+  geom_vline(aes(xintercept = mean(x_ret) - sd(x_ret))) +
+  geom_vline(aes(xintercept = mean(x_ret) + (sd(x_ret) * 2))) +
+  geom_vline(aes(xintercept = mean(x_ret) - (sd(x_ret) * 2)))
 
-ggplot(data = SPY_ret, aes(x = Index , y = SPY_ret$daily.returns)) +
-  geom_line() +
-  geom_hline(
-    yintercept = c(plus1sd, plus2sd, minus1sd, minus2sd),
-    color = "black",
-    linetype = "solid"
-  ) +
-  geom_hline(yintercept = SPY_meanReturn,
-             color = "blue",
-             linetype = "dashed")
-
-summary(SPY_ret)
-
-zscore <- function(x) {
-  round(((-0.17 - mean(x)) / sd(x)), digits = 3)
+zscore <- function(z, p) {
+  round(((p - mean(z)) / sd(z)), digits = 5)
 }
-z_score <- zscore(SPY_ret)
-pnorm(z_score, lower.tail = TRUE)
+z_score <- zscore(x_ret, p = 0.1)
+z_score
+pnorm(z_score, lower.tail = FALSE)
