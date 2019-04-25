2f95fb751a3730ae9b9514ce636e665450e6cf80
murphy-k <kyleevanmurphy@gmail.com>
2019-04-25 13:14:46 -0400

    Added plotly plot for interactive exploration of the sp500 average daily return against volatility.

--- Utilities/Options Pricing.R
+++ Utilities/Options Pricing.R
@@ -7,17 +7,17 @@ dte <- function(n) {
   print(expTime)
 }
 
-n <- as.Date("2019-09-20") - Sys.Date()
+n <- as.Date("2019-05-17") - Sys.Date()
 print(n)
 
-dte(n)
+dte(23)
 
 type <- "c"
-stockPrice <- 85.93
-strikePrice <- 91
+stockPrice <- 30
+strikePrice <- 40
 rateInterest <- 0.00
 borrowRate <- 0.00
-impliedVol <- 0.1844
+impliedVol <- 0.71
 
 # Vanilla Pricing ####
 GBSOption(
--- Utilities/SP500StocksAvgReturns.R
+++ Utilities/SP500StocksAvgReturns.R
@@ -1,32 +1,19 @@
-# If I want exposure to the broad market from the short side - how can I do that
-# with options without paying the large amount of premium on SPY puts?
-# I suspect I could find a stock with a high correlation to SPY that is lower in
-# its price. Solving my issue of exposure to the SPY with lower price premium in
-# the puts.
-rm(list = ls())
 library(quantmod)
-# for scraping
 library(rvest)
-# blanket import for core tidyverse packages
 library(tidyverse)
-# tidy financial analysis
 library(tidyquant)
-# tidy data cleaning functions
 library(janitor)
+library(plotly)
 
-# We have the SPY data but I would like to see which stocks in the entire 505
-# in the SP500 correlate closely with it, I will use a lookback period of three
-# years.
-
+rm(list = ls())
 
 today <- Sys.Date()
-# subtract 3 years from the current date
-# date = today %m+% years(-3)
+
+# subtract 3 months from the current date
+# date = today %m+% months(-3)
 date = "2019-01-01"
 # pass SP500 ticker ^GSPC to tq_get function
 SP500 = tq_get("^GSPC", from = date)
-SP500 %>%
-  head()
 
 # We need to create a vector of all companies in the SP500, iterate over this
 # vector and call tq_get() on each element in the vector, returning a dataframe
@@ -38,7 +25,7 @@ SP500 %>%
 url <- "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
 
 # use that URL to scrape the SP500 table using rvest
-tickers <- url %>%
+sp500_scrape <- url %>%
   # read the HTML from the webpage
   read_html() %>%
   # one way to get table
@@ -46,41 +33,45 @@ tickers <- url %>%
   # easier way to get table
   html_nodes(xpath = '//*[@id="constituents"]') %>%
   html_table()
-#create a vector of tickers
-sp500tickers <- tickers[[1]]
-sp500tickers = sp500tickers %>% mutate(Symbol = case_when(
-  Symbol == "BRK.B" ~ "BRK-B",
-  Symbol == "BF.B" ~ "BF-B",
-  TRUE ~ as.character(Symbol)
-))
+
+#create a vector of sp500_scrape
+sp500_table <- sp500_scrape[[1]]
+
+sp500_table = sp500_table %>%
+  mutate(Symbol = case_when(
+    Symbol == "BRK.B" ~ "BRK-B",
+    Symbol == "BF.B" ~ "BF-B",
+    TRUE ~ as.character(Symbol)
+  ))
+
 get_symbols = function(ticker = "AAPL") {
   df = tq_get(ticker, from = date) %>%
     mutate(symbol = rep(ticker, length(date)))
 }
 
-tickers_df = map(sp500tickers$Symbol, get_symbols) %>%
+# Get all the symbols and place them in a tidy dataframe
+sp500_df = map(sp500_table$Symbol, get_symbols) %>%
   bind_rows()
 
-tickers_df = tickers_df %>%
-  left_join(sp500tickers, by = c('symbol' = 'Symbol')) %>%
+sp500_df = sp500_df %>%
+  left_join(sp500_table, by = c('symbol' = 'Symbol')) %>%
   clean_names() %>%
   select(date:security, gics_sector, gics_sub_industry)
 
-tickers_df %>%
+sp500_df %>%
   select(symbol) %>%
   distinct() %>%
   count() %>%
   select("Total Number of Tickers" = n)
 
 # Viewing an individual ticker's price history
-ticker = "MMM"
-tickers_df %>%
-  filter(symbol == !!ticker) %>%
+sp500_df %>%
+  filter(symbol == !!"AAPL") %>%
   ggplot(aes(date, adjusted)) +
   geom_line()
 
 # Converting the price history to returns
-daily_sector = tickers_df %>%
+daily_sector = sp500_df %>%
   group_by(security, gics_sector, symbol) %>%
   tq_transmute(select     = adjusted,
                mutate_fun = periodReturn,
@@ -92,28 +83,65 @@ daily_sector = tickers_df %>%
 avg_return = daily_sector %>%
   group_by(security, gics_sector) %>%
   summarise(avg_return = round(mean(daily.returns), 4),
-            Volatility =   sd(daily.returns)) %>%
+            Volatility = sd(daily.returns)) %>%
   arrange(desc(avg_return), desc(Volatility))
 avg_return %>% head()
 
 
-# View as a bar plot
-avg_return %>% head(20) %>%
+# View the top 4.95% of SP500 in average daily returns
+avg_return %>%
+  head(25) %>%
   ggplot(aes(reorder(security, -avg_return), avg_return, fill = avg_return)) +
   geom_col() +
   coord_flip() +
-  labs(title = "Highest Average Return Past 3 Years",
+  labs(title = "Highest Average Return",
        x = "Security",
        y = "Average Return") +
   theme_classic() +
   theme(legend.position = "none")
 
+# View the bottom of 4.95% in average daily returns
+avg_return %>%
+  tail(25) %>%
+  ggplot(aes(reorder(security, -avg_return), avg_return, fill = avg_return)) +
+  geom_col() +
+  coord_flip() +
+  labs(title = "Highest Average Return",
+       x = "Security",
+       y = "Average Return") +
+  theme_classic() +
+  theme(legend.position = "none")
 
 # Average returns given volatility
-avg_return %>%
-  ggplot(aes(avg_return, Volatility)) +
-  geom_text(aes(label = security), size = 3) +
-  labs(title = "Average Return vs Volatility Over Last 3 Years In SP500",
+returns_plot <- avg_return %>%
+  ggplot(aes(avg_return, Volatility, color = gics_sector)) +
+  geom_point(alpha = 0, show.legend = TRUE) +
+  geom_text(aes(label = security, color = gics_sector),
+            size = 3,
+            show.legend = FALSE) +
+  labs(title = "Average Return vs Volatility In SP500",
        x = "Average Return",
-       subtitle = "Data Source: Yahoo Finance") +
+       subtitle = paste("Data Start:", date)) +
+  guides(color = guide_legend("Sector",
+                              override.aes = list(size = 2, alpha = 1))) +
   theme_minimal()
+returns_plot
+
+
+plot_ly(
+  avg_return,
+  x = ~ avg_return,
+  y = ~ Volatility,
+  type = 'scatter',
+  mode = 'markers+text',
+  text = ~ security,
+  textposition = 'middle right',
+  color = ~ gics_sector,
+  colors = 'Paired',
+  textfont = list(size = 8)
+) %>%
+  layout(
+    title = 'Average Return vs Volatility In SP500 (Past 3 Months)',
+    xaxis = list(title = 'Average Return', zeroline = TRUE),
+    yaxis = list(title = 'Volatility')
+  )
