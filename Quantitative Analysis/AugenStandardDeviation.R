7ae06f4b839514dac5ddcc0e116a82c0b0201502
Murphy <murphyky18@f5krr0azf694.intra.ecu.edu>
2019-07-16 16:45:20 -0400

    built augen std dev out more

--- Quantitative Analysis/AugenStandardDeviation.R
+++ Quantitative Analysis/AugenStandardDeviation.R
@@ -1,4 +1,7 @@
 library(tidyquant)
+library(timetk)
+library(gridExtra)
+rm(list = ls())
 
 # Augen Standard Deviation Function ####
 slideapply <- function(x, n, FUN = sd) {
@@ -19,9 +22,9 @@ augenSD <- function(x, n = 20) {
   return(augen_sd)
 }
 
-start_date <- "2009-01-01"
+start_date <- "2014-01-01"
 end_date <- Sys.Date()
-ticker <- "SPY"
+ticker <- "GLD"
 
 # Basic Visual ####
 x <-
@@ -29,27 +32,80 @@ x <-
              auto.assign = FALSE,
              from = start_date,
              to = end_date)
-asd <- augenSD(x = as.vector(Cl(x)), n = 90)
+asd <- augenSD(x = as.vector(Cl(x)), n = 20)
 x$augenstdev <- asd
-ggplot(data = x, aes(x = Index, y = x$augenstdev)) + geom_line()
+
+p_stockchart <-
+  ggplot(data = x, aes(x = Index, y = x[,4])) + geom_line()
+
+p_augensd <- ggplot(data = x, aes(x = Index, y = x$augenstdev)) +
+  geom_col() +
+  labs(title = "Daily Std. Dev Moves") +
+  xlab("Date") +
+  ylab("Standard Deviations")
+
+grid.arrange(p_stockchart, p_augensd, ncol = 1)
+
+ggplot(data = x_tbl, aes(x = date, y = logdiff)) + geom_line() +
+  geom_hline(aes(yintercept = mean_logdiff)) +
+  geom_hline(aes(yintercept = mean_logdiff + (sd_logdiff * 2))) +
+  geom_hline(aes(yintercept = mean_logdiff - (sd_logdiff * 2)))
 
 # Adding log difference of actual price change
-x$logdiff <- diff(x = x$SPY.Close,lag = 1,log = TRUE,na.pad = TRUE)
-ggplot(data = x, aes(x=logdiff)) + geom_histogram(bins=100)
-# tidyquant Visual ####
-spy <- tq_get("SPY", from = start_date, to = end_date)
-spy$spike <- asp
-spy %>%
-  filter(date >= as.Date("2007-01-01") &
-           date <= as.Date("2008-01-01")) %>%
-  ggplot(aes(x = date, y = close)) +
-  geom_candlestick(aes(
-    open = open,
-    high = high,
-    low = low,
-    close = close
-  )) +
-  labs(title = "SPY Candlestick Chart", y = "Closing Price", x = "")
+x$logdiff <- diff(
+  x = x[, 4],
+  lag = 1,
+  log = TRUE,
+  na.pad = TRUE
+)
 
+mean_logdiff <- mean(na.trim(x$logdiff))
+sd_logdiff <- sd(na.trim(x$logdiff))
 
-# Viewing SD moves 
+x_tbl <- tk_tbl(x)
+x_tbl <-
+  `colnames<-`(
+    x_tbl,
+    c(
+      "date",
+      "open",
+      "high",
+      "low",
+      "close",
+      "volume",
+      "adj",
+      "augenstdev",
+      "logdiff"
+    )
+  )
+
+x_tbl$outliers <-
+  ifelse(
+    x_tbl$augenstdev >= 2 &
+      x_tbl$logdiff < (mean_logdiff + (sd_logdiff * 2)),
+    1,
+    ifelse(x_tbl$augenstdev <= -2 &
+             x_tbl$logdiff > (mean_logdiff - (sd_logdiff * 2)),
+           -1,
+           0)
+  )
+
+x_tbl %>% filter(date > "2010-01-01") %>%
+  ggplot(aes(x = date, y = close, color = outliers)) +
+  geom_point() +
+  scale_colour_gradient(high = "red", low = "green")
+
+ggplot(data = x_tbl, aes(
+  x = x_tbl$logdiff,
+  y = x_tbl$augenstdev,
+  col = outliers
+)) +
+  geom_point() +
+  labs(title = "Daily Std.Dev Moves vs. Log Difference of Price Change") +
+  xlab("% Price Change") +
+  ylab("Standard Deviations") +
+  geom_vline(aes(xintercept = mean_logdiff + (sd_logdiff * 2))) +
+  geom_vline(aes(xintercept = mean_logdiff - (sd_logdiff * 2))) +
+  geom_hline(aes(yintercept = 2), linetype = 2) +
+  geom_hline(aes(yintercept = -2), linetype = 2)
+
